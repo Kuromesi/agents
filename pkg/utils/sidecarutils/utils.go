a78fc3425d732c2c979d022e7e228e3e0fc83f49
jicheng.sk <jicheng.sk@alibaba-inc.com>
2026-05-30 14:03:36 +0800

    refactor(sidecarutils): pass *Sandbox into doSidecarInjection
    Signed-off-by: jicheng.sk <jicheng.sk@alibaba-inc.com>

--- pkg/utils/sidecarutils/utils.go
+++ pkg/utils/sidecarutils/utils.go
@@ -49,20 +49,12 @@ func InjectSandboxRuntimes(ctx context.Context, sandbox *agentsv1alpha1.Sandbox,
 	return doSidecarInjection(ctx, sandbox, pod, config)
 }
 
-func extractRuntimes(sandbox *agentsv1alpha1.Sandbox) []string {
-	runtimes := []string{}
-	for _, runtime := range sandbox.Spec.Runtimes {
-		runtimes = append(runtimes, runtime.Name)
-	}
-	return runtimes
-}
-
 func doSidecarInjection(ctx context.Context, sandbox *agentsv1alpha1.Sandbox, pod *corev1.Pod, injectConfigMap map[string]string) error {
 	logger := logf.FromContext(ctx)
 
-	runtimes := extractRuntimes(sandbox)
 	injectedRuntimes := sets.NewString()
-	for _, runtime := range runtimes {
+	for _, r := range sandbox.Spec.Runtimes {
+		runtime := r.Name
 		if injectedRuntimes.Has(runtime) {
 			logger.V(5).Info("skipping duplicate runtime, already processed", "runtime", runtime)
 			continue
